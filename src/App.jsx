d5014c6f2dd1c3c33e6ebdb1079454604450defc
Otabekcdr <boltaboyevotabek207@gmail.com>
2025-02-18 10:58:07 +0500

    validation

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <>
             <HomeComponent />
-            <ToastContainer />
+            <ToastContainer position="bottom-right" />
         </>
     )
 }
